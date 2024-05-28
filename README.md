4d8e7617399d5bf968dc928b2550ddbfa62cd641
ismeh <gadeismael@hotmail.com>
2024-05-28 15:29:52 +0200

    Script & Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Para cambiar de plan de energía primero debemos crear un plan extra, para ello seguiremos el 
+
+
+Bibliografía
+	- Fuente del icono: https://icon-icons.com/download/41592/ICO/512/
+	- Anclar archivos: https://answers.microsoft.com/es-es/windows/forum/all/como-anclar-un-acceso-directo-que-da-a-un-archivo/4f9b0800-f0b2-44a3-b32c-8772aeb091a9
+	- Limitar consumo de energía: https://www.youtube.com/watch?v=Hgx0Q7Yjfis
+	
---
+++ cambiar_plan_energia.bat
@@ -0,0 +1,22 @@
+rem Para cambiar el plan de energía mediante comandos tenecesitamos conocer el identificador del plan (GUID), usaremos 'powercfg /list' desde la terminal para ver los planes disponibles. En este caso he hardcodeado los GUID de dichos planes.
+
+set plan_equilibrado=381b4222-f694-41f0-9685-ff5bb260df2e
+set plan_bajo_rendimiento=b54f4c31-cb06-4429-b20a-9c60c8fae77e
+
+rem Obtener el plan actual
+for /f "tokens=6" %%a in ('powercfg /getactivescheme') do set plan_actual=%%a
+
+if "%plan_actual%"=="%plan_equilibrado%" (
+	rem Notificación del cambio
+    msg * /time:1 "Cambiando al plan de bajo consumo."
+
+    rem Cambio del plan
+    powercfg /S %plan_bajo_rendimiento%    
+) else (
+    msg * /time:1 "Cambiando al plan equilibrado."
+    powercfg /S %plan_equilibrado%
+)
+
+
+rem timeout /t 50 >nu
+
